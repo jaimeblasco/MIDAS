fc1479443ccff1ae188dc7bb705052da8ce62ff3
Bill Smartt <bsmartt@alienvault.com>
2014-02-14 11:28:15 -0800

    alienvault_integration/midas.sql: Update name in ossim-db so events in siem are more accurate and explicit.

--- alienvault_integration/midas.sql
+++ alienvault_integration/midas.sql
@@ -6,6 +6,6 @@
 
 		INSERT INTO plugin (id, type, name, description) VALUES (1987, 1, 'MIDAS', 'MIDAS Macosx IDS');
 
-		INSERT INTO plugin_sid (plugin_id, sid, category_id, class_id, name, priority, reliability) VALUES (1987, 1, NULL, NULL, 'MIDAS: New username' ,1, 1);
-		INSERT INTO plugin_sid (plugin_id, sid, category_id, class_id, name, priority, reliability) VALUES (1987, 2, NULL, NULL, 'MIDAS: New kernel extension' ,1, 1);
-		INSERT INTO plugin_sid (plugin_id, sid, category_id, class_id, name, priority, reliability) VALUES (1987, 3, NULL, NULL, 'MIDAS: New application pslist' ,1, 1);
+		INSERT INTO plugin_sid (plugin_id, sid, category_id, class_id, name, priority, reliability) VALUES (1987, 1, NULL, NULL, 'MIDAS: New User' ,1, 1);
+		INSERT INTO plugin_sid (plugin_id, sid, category_id, class_id, name, priority, reliability) VALUES (1987, 2, NULL, NULL, 'MIDAS: New kernel extension (OS X kext)' ,1, 1);
+		INSERT INTO plugin_sid (plugin_id, sid, category_id, class_id, name, priority, reliability) VALUES (1987, 3, NULL, NULL, 'MIDAS: New Property List (OS X plist)' ,1, 1);
