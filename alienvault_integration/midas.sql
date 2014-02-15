252a9515189ec5972741b7dafe51d39572b8688f
Bill Smartt <bsmartt@alienvault.com>
2014-02-15 07:01:15 -0800

    alienvault_integration/midas.cfg: expand each of (user, plist, kext) into 3: new, changed, removed. alienvault_integration/midas.sql: add sql defs to match new plugin layout.

--- alienvault_integration/midas.sql
+++ alienvault_integration/midas.sql
@@ -6,7 +6,11 @@
 
 		INSERT INTO plugin (id, type, name, description) VALUES (1987, 1, 'MIDAS', 'MIDAS Macosx IDS');
 
-		INSERT INTO plugin_sid (plugin_id, sid, category_id, class_id, name, priority, reliability) VALUES (1987, 1, NULL, NULL, 'MIDAS: New User account' ,1, 1);
-		INSERT INTO plugin_sid (plugin_id, sid, category_id, class_id, name, priority, reliability) VALUES (1987, 2, NULL, NULL, 'MIDAS: Removed User account' ,1, 1);
-		INSERT INTO plugin_sid (plugin_id, sid, category_id, class_id, name, priority, reliability) VALUES (1987, 3, NULL, NULL, 'MIDAS: New Kernel Extension (OS X kext)' ,1, 1);
-INSERT INTO plugin_sid (plugin_id, sid, category_id, class_id, name, priority, reliability) VALUES (1987, 4, NULL, NULL, 'MIDAS: New Property List (OS X plist)' ,1, 1);
+		INSERT INTO plugin_sid (plugin_id, sid, category_id, class_id, name, priority, reliability) VALUES (1987, 1, NULL, NULL, 'MIDAS: Removed User account' ,1, 1);
+		INSERT INTO plugin_sid (plugin_id, sid, category_id, class_id, name, priority, reliability) VALUES (1987, 2, NULL, NULL, 'MIDAS: New User account' ,1, 1);
+		INSERT INTO plugin_sid (plugin_id, sid, category_id, class_id, name, priority, reliability) VALUES (1987, 3, NULL, NULL, 'MIDAS: Removed Kernel Extension (OS X kext)' ,1, 1);
+		INSERT INTO plugin_sid (plugin_id, sid, category_id, class_id, name, priority, reliability) VALUES (1987, 4, NULL, NULL, 'MIDAS: Changed Kernel Extension (OS X kext)' ,1, 1);
+		INSERT INTO plugin_sid (plugin_id, sid, category_id, class_id, name, priority, reliability) VALUES (1987, 5, NULL, NULL, 'MIDAS: New Kernel Extension (OS X kext)' ,1, 1);
+    INSERT INTO plugin_sid (plugin_id, sid, category_id, class_id, name, priority, reliability) VALUES (1987, 6, NULL, NULL, 'MIDAS: Removed Property List (OS X plist)' ,1, 1);
+    INSERT INTO plugin_sid (plugin_id, sid, category_id, class_id, name, priority, reliability) VALUES (1987, 7, NULL, NULL, 'MIDAS: Changed Property List (OS X plist)' ,1, 1);
+    INSERT INTO plugin_sid (plugin_id, sid, category_id, class_id, name, priority, reliability) VALUES (1987, 8, NULL, NULL, 'MIDAS: New Property List (OS X plist)' ,1, 1);
